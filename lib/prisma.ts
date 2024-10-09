d71297fdf484b9d9ef02ca3c18c353d7ec8e20bb
amjido-01 <youndsadeeq10@gmail.com>
2024-10-09 21:12:27 +0100

    commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,7 +9,6 @@ type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined
 }
-
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 export default prisma
