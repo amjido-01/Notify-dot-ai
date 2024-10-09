2be2345454745f0e5b38e4af202beed222331ec3
amjido-01 <youndsadeeq10@gmail.com>
2024-10-09 22:05:27 +0100

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { useSession } from "next-auth/react"
-import { useRouter } from "next/navigation"
+// import { useRouter } from "next/navigation"
  
 export default function Dashboard() {
   const { data: session } = useSession()
-  const router = useRouter()
+  // const router = useRouter()
  
   return (
     <p>You are, welcome! {session?.user?.name}</p>
