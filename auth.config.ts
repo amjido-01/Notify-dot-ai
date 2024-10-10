4e5786f52a51c0ff0fa410b5f527ac47e75c7463
amjido-01 <youndsadeeq10@gmail.com>
2024-10-10 06:33:53 +0100

    commit

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,7 @@ export default {
         ({
           clientId: process.env.AUTH_GOOGLE_ID!,
           clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+          allowDangerousEmailAccountLinking: true,
         }),
       ]
 } satisfies NextAuthConfig
