60a9792111f201310667b7b9fe9f90cde0f49f25
amjido-01 <youndsadeeq10@gmail.com>
2024-10-10 06:22:03 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { cookies } from "next/headers"
 import { decrypt } from "./lib/session"
  
 // export const { auth: middleware } = NextAuth(authConfig)
-
+// please
 const protectedRoutes = ["/dashboard", "/interest", "/profile", "auth/setting"]
 const publicRoutes = ["/auth/signin", "/auth/signup", "/"] 
 
