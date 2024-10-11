7e222ee0d98691c5fac56c28b8107dee499a8594
amjido_01 <96017433+amjido-01@users.noreply.github.com>
2024-10-11 04:59:51 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,24 @@
+
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+
+ 
+export const { auth: middleware } = NextAuth(authConfig)
+
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+  }
+
 // import NextAuth from "next-auth"
 // import authConfig from "./auth.config"
 // import { auth } from "./auth"
